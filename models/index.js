4b8d59d9b6c59594f054ba2dc1896d4ffe4a7cf7
Isaac Madwed <i.madwed@gmail.com>
2015-07-01 17:56:07 -0400

    fixed schemas

--- models/index.js
+++ models/index.js
@@ -16,7 +16,7 @@ var placeSchema = new mongoose.Schema({
 
 var hotelSchema = new mongoose.Schema({
 	name: String,
-	place: String,
+	place: [placeSchema],
 	//Integer
 	num_stars: { type: Number, min: 1, max: 5 } ,
 	//Comma delineated vvv
@@ -25,13 +25,13 @@ var hotelSchema = new mongoose.Schema({
 
 var thingToDoSchema = new mongoose.Schema({
 	name: String,
-	place: String,
+	place: [placeSchema],
 	age_range: String
 });
 
 var restaurantSchema = new mongoose.Schema({
 	name: String,
-	place: String,
+	place: [placeSchema],
 	//Comma delineated vvv
 	cuisine: String,
 	//For number of $ - $$$$$
