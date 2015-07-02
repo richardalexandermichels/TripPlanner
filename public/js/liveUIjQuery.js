00f2089ec6d86f362d41e5d8f52f03a81c132a6e
Kelsey <kelseysroberts@gmail.com>
2015-07-02 18:29:59 -0400

    button functionality

--- public/js/liveUIjQuery.js
+++ public/js/liveUIjQuery.js
@@ -1,9 +1,67 @@
-$(".btn-primary").on("click", function(){
-	var contents = $(this).prev().val();
-	//console.log("some content !",$(this).prev().val())
-	$(".hotel-list ul").append("<li>"+contents+"</li>")
+var daysArray = [{}];
+$(".hotel-button").on("click", function(){
+	var day = daysArray[0];
+	if (!day.hotel)
+	 day.hotel = $(this).prev().val();
+	console.log(daysArray);
 })
 
 
-// <li>"+
-// 		$(this).sibling("select").child().val()+"</li>
+
+
+
+$(".hotel-button").on("click", function(){
+	var contents = '<div class="itinerary-item"> <span class="title">'+$(this).prev().val()+'</span> <button class="btn btn-xs btn-danger remove btn-circle hotel-delete">x</button> </div>';
+		console.log("firstChild: ",$(".hotel-list ul").firstChild)
+		if (!daysArray[0].hotel){
+			$(".hotel-list ul").append("<li>"+contents+"</li>")
+		} else {
+			$(".hotel-list ul").closest("<li>").remove();
+			$(".hotel-list ul").append("<li>"+contents+"</li>")
+		}
+})
+
+
+$(".restaurant-button").on("click", function(){
+		var contents = '<div class="itinerary-item"> <span class="title">'+$(this).prev().val()+'</span> <button class="btn btn-xs btn-danger remove btn-circle pull-right restaurant-delete">x</button> </div>';
+	$(".restaurant-list").append("<li>"+contents+"</li>");
+})
+
+$(".thingstodo-button").on("click", function(){
+		var contents = '<div class="itinerary-item"> <span class="title">'+$(this).prev().val()+'</span> <button class="btn btn-xs btn-danger remove btn-circle pull-right thingstodo-delete">x</button> </div>';
+	$(".thingstodo-list").append("<li>"+contents+"</li>");
+})
+
+
+
+
+
+
+
+$(".hotel-list").delegate(".hotel-delete", "click", function(){
+	$(this).closest($("li")).remove();
+	$(this).remove();
+})
+
+$(".restaurant-list").delegate(".restaurant-delete", "click", function(){
+	$(this).closest($("li")).remove();
+	$(this).remove();
+})
+
+$(".thingstodo-list").delegate(".thingstodo-delete", "click", function(){
+	$(this).closest($("li")).remove();
+	$(this).remove();
+})
+
+
+
+
+
+var count = 0;
+$("#add-day").on("click", function(){
+	count ++
+	console.log($(this).siblings()[0])
+	var selector = ".current-" + count;
+	var classSet = "current-" + Number(count + 1);
+	$(selector).after('<button class="btn btn-circle day-btn '+classSet+'">'+Number(count+1)+'</button>')
+})
